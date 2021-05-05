6adf724a61ae1fc33718f8e549f6c4c081ff0484
OtavioCleyderman <otavio.faria01@fatec.sp.gov.br>
2021-05-05 14:03:33 +0000

    (05/05) Témino do layout básico e introdução do router

--- karangos/src/App.js
+++ karangos/src/App.js
@@ -29,7 +29,10 @@ import yellow from '@material-ui/core/colors/yellow';
 import red from '@material-ui/core/colors/red';
 import TopBar from './ui/TopBar';
 import FooterBar from './ui/FooterBar';
-import Box from '@material-ui/core/Box';
+import Box from '@material-ui/core/Box';  
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import KarangosList from './routed/KarangosList';
+import KarangosForm from './routed/KarangosForm';
 
 const theme = createMuiTheme({
   palette: {
@@ -47,15 +50,36 @@ const useStyles = makeStyles((theme) => ({
   main: {
     backgroundColor: theme.palette.background.default,
     minHeight: '100vh', // 100% da altura da área de visualização
-    }
+    },
+  routed: {
+    padding: '0 25px',
+    textAlign: 'center',
+    color: theme.palette.text.primary,
+    fontFamily: theme.typography.fontFamily,
+  }  
 }))
 
 function Main() {
   const classes = useStyles()
   return(
     <Box className={classes.main}>
-    <TopBar /> 
+      <BrowserRouter> {/* Inicia a região onde pode haver troca dinâmica de elementos */}
+      <TopBar /> 
+        <Box id="routed" className={classes.routed}>
+          <Switch> {/* Determina qual elemento será exibido, de acordo com a rota */}
+
+            <Route path="/list"> {/* Inserindo a rota */}
+              <KarangosList />
+            </Route>
+
+            <Route path="/new"> {/* Inserindo a rota */}
+              <KarangosForm />
+            </Route>
+
+          </Switch>
+        </Box>
       <FooterBar />
+      </BrowserRouter>
     </Box>
   )
 }
